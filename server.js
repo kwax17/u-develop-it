e5ee31a37204cdf29eef0cb9e0704ad7512a4093
Kelly Wacker <kelwacker@gmail.com>
2022-07-29 14:27:34 -0400

    routes

---
+++ server.js
@@ -0,0 +1,21 @@
+// dependenceis
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+// Default response for any other request (Not Found) // SHOULD BE LAST
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// listening at port
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
